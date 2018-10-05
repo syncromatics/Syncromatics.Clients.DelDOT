ed7fd04d9f3bbd43f50d14902ac0d2e4e2763bef
Kenneth Ito <kenneth.ito@gmail.com>
2018-10-05 13:50:57 -0700

    Initial implementation, closes #1

---
+++ src/Syncromatics.Clients.DelDOT.Api/ArrivalTime.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncromatics.Clients.DelDOT.Api
+{
+    public class ArrivalTime
+    {
+        /*{
+            "serviceAvailable": true,
+            "busDetails": false,
+            "destination": "Rehoboth Boardwalk",
+            "busSchArrival": "06:00PM",
+            "busSchCountdown": 4,
+            "busEstArrival": "06:00PM",
+            "busEstCountdown": 4,
+            "tripId": "396616",
+            "stopAlert": false
+        }*/
+
+        public bool ServiceAvailable { get; set; }
+        public bool BusDetails { get; set; }
+        public string Destination { get; set; }
+        [JsonProperty("busEstCountdown")]
+        public int EstimatedArrivalMinutes { get; set; }
+        [JsonProperty("busSchCountdown")]
+        public int ScheduledArrivalMinutes { get; set; }
+        public string TripId { get; set; }
+        public bool StopAlert { get; set; }
+    }
+}
---
+++ src/Syncromatics.Clients.DelDOT.Api/DelDOTClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RestEase;
+
+namespace Syncromatics.Clients.DelDOT.Api
+{
+    public class DelDOTClient : IDelDOTClient
+    {
+        private readonly IDelDOTClient _client;
+
+        public DelDOTClient() 
+            : this(new ClientSettings())
+        {
+
+        }
+
+        public DelDOTClient(ClientSettings clientSettings)
+        {
+            var handler = new HttpClientHandler { MaxConnectionsPerServer = clientSettings.MaxConnections };
+            var httpClient = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(clientSettings.ServerRootUrl)
+            };
+
+            _client = new RestClient(httpClient).For<IDelDOTClient>();
+        }
+
+        public Task<StopArrivals> GetStopArrivalsAsync(int stopId)
+        {
+            return _client.GetStopArrivalsAsync(stopId);
+        }
+    }
+}
